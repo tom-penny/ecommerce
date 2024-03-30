4cbc08efbd41d4ee9fcafd99ce00c14d5825e85f
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-30 15:32:40 +0000

    create basket reducer

---
+++ src/reducers/basket.reducer.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import * as api from '../api/order.api.js'
+
+export const checkoutBasket = createAsyncThunk(
+    'basket/checkoutBasket',
+    async ({ checkoutId, userId, total, basket }) => {
+        const response = await api.createOrder(checkoutId, userId, total, basket)
+        return response.data
+    }
+)
+
+const basketSlice = createSlice({
+    name: 'basket',
+    initialState: { basket: {}, checkoutId: null, status: 'idle', error: null },
+    reducers: {
+        setBasket: (state, action) => {
+            state.basket = action.payload
+        },
+        resetBasket: (state) => {
+            state.basket = {}
+        },
+        resetCheckout: (state) => {
+            state.checkoutId = null
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(checkoutBasket.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(checkoutBasket.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                state.checkoutId = action.payload.checkoutId
+            })
+            .addCase(checkoutBasket.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+    }
+})
+
+export const { setBasket, resetBasket, resetCheckout } = basketSlice.actions
+
+export default basketSlice.reducer

27d03c2417fc0327b2faa609a3c417eba441ea27
w1855014 <w1855014@my.westminster.ac.uk>
2024-03-30 16:47:04 +0000

    create authentication hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+const useAuth = () => {
+    
+    const navigate = useNavigate()
+    const location = useLocation()
+    
+    const { userId } = useSelector(state => state.user)
+
+    const isAuthenticated = !!userId
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/login', { state: { from: location }})
+        }
+    }, [userId, navigate, location])
+
+    return isAuthenticated
+}
+
+export default useAuth

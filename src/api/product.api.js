e55074dd524d9d5e2067512585992ae64f809920
w1855014 <w1855014@my.westminster.ac.uk>
2024-04-01 00:44:51 +0100

    create product api client

---
+++ src/api/product.api.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export const getAllCategories = () => {
+    return axios.get('/api/categories', { withCredentials: true })
+}
+
+export const getProductById = (id) => {
+    return axios.get(`/api/products/${id}`, { withCredentials: true })
+}
+
+export const getAllProducts = (page = 1, sort = 'name', order = 'asc') => {
+    return axios.get(`/api/products?page=${page}&size=${12}&sort=${sort}&order=${order}`, { withCredentials: true })
+}
+
+export const getProductsByCategory = (slug, page = 1, sort = 'name', order = 'asc') => {
+    return axios.get(`/api/categories/${slug}/products?page=${page}&size=${12}&sort=${sort}&order=${order}`, { withCredentials: true })
+}

1f9de0d61e5e8a6ca0879006036d548e5d0fc229
w1855014 <w1855014@my.westminster.ac.uk>
2024-04-01 01:02:52 +0100

    add app router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import classNames from 'classnames'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchUser } from './reducers/user.reducer'
+
+import './App.scss'
+
 function App() {
 
-    return <div></div>
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUser())
+    }, [dispatch])
+
+    return <div className={classNames('app', { 'app--locked': menuOpen })}>
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    </div>
 }
 
 export default App

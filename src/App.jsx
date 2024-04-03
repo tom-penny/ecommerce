fde8a3a39d97259ccbb8079eea414996ccd0e72f
w1855014 <w1855014@my.westminster.ac.uk>
2024-04-03 13:40:28 +0100

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage, NotFoundPage, LoginPage, RegisterPage } from './pages'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchUser } from './reducers/user.reducer'
@@ -17,7 +18,10 @@ function App() {
     return <div className={classNames('app', { 'app--locked': menuOpen })}>
         <BrowserRouter>
             <Routes>
-                
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='/login' element={<LoginPage/>}/>
+                <Route path='/register' element={<RegisterPage/>}/>
+                <Route path='/*' element={<NotFoundPage/>}/>
             </Routes>
         </BrowserRouter>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+export { default as HomePage } from './HomePage/HomePage'
 export { default as LoginPage } from './LoginPage/LoginPage'
 export { default as NotFoundPage } from './NotFoundPage/NotFoundPage'
 export { default as RegisterPage } from './RegisterPage/RegisterPage'

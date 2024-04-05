fada774c3bcedd1e20a28957764ad9bd4a5eb9d7
w1855014 <w1855014@my.westminster.ac.uk>
2024-04-05 22:44:32 +0100

    create product card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/Header'
 export { default as Pagination } from './Pagination/Pagination'
+export { default as ProductCard } from './ProductCard/ProductCard'
 export { default as Quantifier } from './Quantifier/Quantifier'

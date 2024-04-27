0abcf885bcc8b589d481f53392f5e39759ef42a9
w1855014 <w1855014@my.westminster.ac.uk>
2024-04-27 22:04:39 +0100

    update not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from 'react-router-dom'
+import { EmptyState } from '../../components'
+
 import './NotFoundPage.scss'
 
 const NotFoundPage = () => {
+    const navigate = useNavigate()
 
     return <div className='not-found-page'>
-        <></>
+        <EmptyState heading='404' message='Page not found.'>
+            <button className='not-found-page__btn' onClick={() => navigate('/catalogue')}>Shop now</button>
+        </EmptyState>
     </div>
 }
 
